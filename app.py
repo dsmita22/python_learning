a530acd8dbd48c5464903220f025c6ec80edab36
smita <dsmita22@gmail.com>
2021-05-06 00:01:24 +1000

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+print('Smita')
+print('*' * 10)
+
+#Defind veriable
+price = 10
+print('ex 1 :', price)
+
+#update value of veriable
+price = 10
+price = 20
+print('ex 2 :', price)
+
+# We check in a patient named John Smith.He's 20 trs old and is a new patient
+patient_name = 'JOhn Smith'
+patient_age = 20
+is_new = True
+
+#take the input and print
+name = input('What is your name? ')
+print('Hi ' + name)
+
+#ask 2 question: person's name and fav colour, then print message like 'Smita likes blue'
+name1 = input('what is your name? ')
+colour = input('what is your favourite colour? ')
+print(name1+' likes '+colour)
+
+#ask a user their weight (in pound) then convert it to kilograms and print on terminal
+weight_lbs = input('what is your weight? ')
+print(type(weight_lbs))
+weight_kg = int(weight_lbs) * 0.45
+print(type(weight_kg))
+print('Weight in KG ' + str(weight_kg))
+
+
